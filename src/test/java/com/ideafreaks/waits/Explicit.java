839312f2a88bed52c840e26b0a686229a7ff10f9
noahclow <noahclow@icloud.com>
2022-03-09 23:57:06 -0600

    Add ExplicitWait

--- src/test/java/com/ideafreaks/waits/Explicit.java
+++ src/test/java/com/ideafreaks/waits/Explicit.java
@@ -1,11 +1,48 @@
 package com.ideafreaks.waits;
 
+import java.io.IOException;
+import java.time.Duration;
+
 import com.ideafreaks.resources.Base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
 public class Explicit extends Base{
+    String URL = "https://demoqa.com/dynamic-properties";
 
-    String url = "https://demoqa.com/dynamic-properties";
+    public WebDriver driver;
+    public String status;
 
-    
-    
+    public static Logger log = (Logger) LogManager.getLogger(Base.class.getName());
+
+    @Test(description = "Explicit Wait", enabled = true)
+    protected void explicitWait() throws InterruptedException, IOException {
+
+
+
+        driver = initializeDriver();
+        log.info("Driver is initialized");
+        driver.get(URL);
+        log.info("Open " + URL);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("visibleAfter")));
+            driver.findElement(By.id("visibleAfter")).click();
+            System.out.println("Boton clickeado");
+        } catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        driver.quit();
+    }
+
 }
--- src/test/java/com/ideafreaks/waits/Implicit.java
+++ src/test/java/com/ideafreaks/waits/Implicit.java
@@ -9,8 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Implicit extends Base{
@@ -38,6 +36,7 @@ public class Implicit extends Base{
         {
             System.out.println(e);
         }
+        driver.quit();
     }
 
 }
